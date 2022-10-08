2c15c121dc7d88787e2e28f0ef0440f5343d93f1
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-10-09 00:06:48 +0200

    ADD/trying filters

--- client/src/components/Main/Landing/Landing.jsx
+++ client/src/components/Main/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { landingContext } from '../../../context/landingContext';
 import axios from 'axios';
 
@@ -7,10 +7,15 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 const Landing = () => {
-    const { landings, setLandings } = useContext(landingContext)
+    const [ landings, setLandings ] = useState("")
 
+    //estados para Search
+    const [filterMap, setfilterMap] = useState(null);
+    const [select, setSelect] = useState(null);
+    const [option, setOption] = useState(null);
+    
     const landIcon = new L.Icon({
-        iconUrl: require('../../../assets/gif/land.gif'),
+        iconUrl: require('../../../assets/img/cometa.png'),
         iconSize: new L.Point(25, 25),
     })
 
@@ -21,11 +26,9 @@ const Landing = () => {
 
     const getLandings = async () => {
         try {
-            if (landings.length === 0) {
                 const { data } = await axios.get(`http://localhost:5000/api/astronomy/landings`);
                 //data es del destructuring del fetch
                 setLandings(data) //seteamos el estado con 'data' del fetch 
-            }
             
         } catch (error) {
             console.log(error)
@@ -35,39 +38,63 @@ const Landing = () => {
 
 
     //SEARCH LANDINGS:
+
+    useEffect((select, option) => {
+        getFilteredMap(select, option)
+    }, [select, option, filterMap])
+
+
+    const getFilteredMap = async () => {
+        try {
+
+            const { data } = await axios.get(`http://localhost:5000/api/astronomy/landings/${select}/${option}`);
+            setLandings(data)
+            setfilterMap(data)
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+
+
     const handleSubmit = (e) =>{
         e.preventDefault();
-        const select = e.target.by.value;
-        const option = e.target.option.value;
-        const mayusOption = option.toUpperCase();
+        const select = e.target.select.value;
+        const input = e.target.input.value;
+        const mayusOption = input.toUpperCase();
+        
         setOption(mayusOption);
         setSelect(select);
+        setLandings(filterMap);
     
-        e.target.option.value="";
+        e.target.input.value = "";
     
       }
 
 
+    if (landings) {
     return (
         <>
          <div>
           <h4>Search landings!</h4>
           <form onSubmit={handleSubmit}>
-            <select name="by">
-                <menuitem value="mass">By weight</menuitem>
-                <menuitem value="class">By class</menuitem>
+            <select name="select">
+                <option value="mass">By mass</option>
+                <option value="class">By class</option>
             </select>
-            <TextField id="outlined-basic" label={select} variant="outlined" name="option"/>
-            <button variant="outlined" type="submit">Submit</button>
+            <input id="outlined-basic" label={select} variant="outlined" name="input"/>
+            <button variant="outlined" type="submit">Search</button>
           </form>
         </div>
 
-        <div>
+        {/* <div>
           <h4>Check all landings</h4>
           <button variant="outlined" onClick={handleClick}>List</button>
-        </div>
+        </div> */}
 
-            <MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={true} style={{ width: '100%', height: '500px' }}>
+            <MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={true} style={{ width: '100%', height: '500px', marginTop:'30px' }}>
                 <TileLayer
                     attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
                     url="https://api.mapbox.com/styles/v1/mogar99/cl8w4411n000j15prntrktrgw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibW9nYXI5OSIsImEiOiJja2Z3ZDJoaGQxOXFqMzN0OHBhajdjMXBxIn0.1-1aPslRK9n1m1QAS20q3g"
@@ -90,7 +117,52 @@ const Landing = () => {
             </MapContainer>
 
         </>
-    )
+    )}
+    else if (filterMap) {
+        return (
+        <>
+         <div>
+          <h4>Search landings!</h4>
+          <form onSubmit={handleSubmit}>
+            <select name="select">
+                <option value="mass">By weight</option>
+                <option value="class">By class</option>
+            </select>
+            <input id="outlined-basic" label={select} variant="outlined" name="input"/>
+            <button variant="outlined" type="submit">Submit</button>
+          </form>
+        </div>
+
+        {/* <div>
+          <h4>Check all landings</h4>
+          <button variant="outlined" onClick={handleClick}>List</button>
+        </div> */}
+
+            <MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={true} style={{ width: '100%', height: '500px', marginTop:'30px' }}>
+                <TileLayer
+                    attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
+                    url="https://api.mapbox.com/styles/v1/mogar99/cl8w4411n000j15prntrktrgw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibW9nYXI5OSIsImEiOiJja2Z3ZDJoaGQxOXFqMzN0OHBhajdjMXBxIn0.1-1aPslRK9n1m1QAS20q3g"
+                />
+
+                {filterMap.map((landData, i) => landData.geolocation && landData.reclat && landData.reclong ? (
+
+                    <Marker position={[String(landData.reclat), String(landData.reclong)]} key={i} icon={landIcon} >
+                        <Popup>
+                            <ul>
+                                <li><h3>{landData.name}</h3></li>
+                                <li><p>ID: {landData.id}</p></li>
+                                <li><p>Nametype: {landData.nametype}</p></li>
+                                <li><p>Reclass: {landData.recclass}</p></li>
+                                <li><p>Mass: {landData.mass}</p></li>
+                                <li><p>Year: {landData.year}</p></li>
+                            </ul>                    
+                        </Popup>
+                    </Marker>) : null)}
+            </MapContainer>
+
+        </>
+        )
+    }
 }
 
 
