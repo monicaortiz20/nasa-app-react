0ca0e4dd53f9886455909a20cc7d6606df3e3bcc
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-10-09 19:34:25 +0200

    ADD/edit and delete

--- client/src/components/Main/Landing/ListLanding/CardLanding/CardLanding.jsx
+++ client/src/components/Main/Landing/ListLanding/CardLanding/CardLanding.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import axios from 'axios';
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { useForm } from "react-hook-form";
 
 const CardLanding = (data) => {
   const landing = data.data
 
+  const { register, handleSubmit } = useForm(); //instalamos librería para FORM
 
 
   //borramos del back por ID:
   const deleteLanding = async () => {
     try {
-      const response  = await axios.delete(`http://localhost:5000/api/astronomy/landings/delete/${landing.id}`)
+      const response = await axios.delete(`http://localhost:5000/api/astronomy/landings/delete/${landing.id}`)
       const info = await response.data;
       console.log('esto es info', info)
       console.log('borramos:', landing.id);
@@ -20,10 +23,37 @@ const CardLanding = (data) => {
     }
   }
 
+  //editamos por ID:
+  const editLanding = async (editLand) => {
+    try {
+      const editLandObj = {
+        name: editLand.name,
+        id: editLand.id,
+        mass: editLand.mass,
+        recclass: editLand.recclass,
+        year: editLand.year,
+        reclat: editLand.reclat,
+        reclong: editLand.reclong,
+        geolocation: {
+          latitude: editLand.reclat,
+          longitude: editLand.reclong
+        }
+      }
+      console.log('esto es el front obj', editLandObj)
+      //hacemos fetch y pasamos obj editado:
+      const response = await axios.put(`http://localhost:5000/api/astronomy/landings/update/${landing.id}`, editLandObj)
+      const info = response.data;
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
-    <section>
-       <div>
+    <>
+      <section>
+        <div>
           <h3>{landing.name}</h3>
           <p>ID: {landing.id}</p>
           <p>Nametype: {landing.nametype}</p>
@@ -34,9 +64,26 @@ const CardLanding = (data) => {
           <p>Reclat: {landing.reclat}</p>
           <p>Reclong: {landing.reclong}</p>
           <button onClick={deleteLanding}>Delete</button>
-          <button>Edit</button>
         </div>
-    </section>
+      </section>
+      <Popup trigger={<button>Edit</button>} position="bottom left">
+        {close => (
+          <div>
+            <form onSubmit={handleSubmit(editLanding)}>
+              <input {...register("name")} placeholder="Name*" label="Name" name="name" required/>
+              <input {...register("id")} placeholder="ID*" label="ID" name="id" required/>
+              <input {...register("recclass")} placeholder="Recclass" label="Class"  name="recclass" />
+              <input {...register("mass")} placeholder="Mass" label="Weight" name="mass"/>
+              <input {...register("year")} placeholder="Year" type="date"  name="year" />
+              <input {...register("reclat")} placeholder="Latitude*" label="Latitude" name="reclat" required/>
+              <input {...register("reclong")} placeholder="Longitude*" label="Longitude" name="reclong" required/>
+              <button type="submit">Edit</button>
+            </form>
+          </div>
+        )}
+
+      </Popup>
+    </>
   )
 }
 
