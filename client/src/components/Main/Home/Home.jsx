47e6ae2b69a5d1c963051002afd855c71185cf54
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-10-10 04:25:07 +0200

    ADD/css done

--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
                 </div>
 
                 <div>
-                    <img className="relative z-10 object-cover w-full rounded-md h-96" src={home.url} alt={home.title} />
+                    <img className="relative z-10 object-cover w-full rounded-md h-90" src={home.url} alt={home.title} />
 
                     <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-gray-900">
                         <a className="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
